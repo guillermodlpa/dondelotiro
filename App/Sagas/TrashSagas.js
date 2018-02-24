0ae8f03a4af7e3e98adc060fbe4ab719b3191b9d
Mario Juarez <mario@mjp.one>
2018-02-24 16:01:56 +0100

    Call to API

--- App/Sagas/TrashSagas.js
+++ App/Sagas/TrashSagas.js
@@ -1,30 +1,33 @@
 import { call, put, select } from 'redux-saga/effects'
 import TrashActions from '../Redux/TrashRedux'
 import { NavigationActions } from 'react-navigation';
+import Secrets from 'react-native-config'
+import axios from 'axios'
 
-export function * requestLocations (action) {
+export function* requestLocations(action) {
   const { trashTypes, geoPosition } = action
 
-  yield put(TrashActions.locationsSuccess([
+  const bodyFormData = new FormData();
+  bodyFormData.append('lat', geoPosition.latitude);
+  bodyFormData.append('lon', geoPosition.longitude);
+  bodyFormData.append('trash_types', trashTypes.join(','));
+
+  const response = yield axios.post(
+    Secrets.API_URL,
+    bodyFormData,
     {
-      trash_types: ['batteries', 'furniture'],
-      container_type: 'clean_point',
-      latitude: 40.3578177,
-      longitude: -3.6975403,
-    },
-    {
-      trash_types: ['dog_shit'],
-      container_type: 'dog_shit_trash',
-      latitude: 40.3588177,
-      longitude: -3.6475403,
-    },
-    {
-      trash_types: ['electronics'],
-      container_type: 'clean_point',
-      latitude: 40.3408177,
-      longitude: -3.5975403,
-    }
-  ]))
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+    .then(response => response)
+    .catch(error => error)
+
+  if (response.data) {
+    yield put(TrashActions.locationsSuccess(response.data.dist))
+  } else {
+    yield put(TrashActions.locationsFailure(error))
+  }
 
   yield put(NavigationActions.navigate({
     routeName: 'MapScreen'
