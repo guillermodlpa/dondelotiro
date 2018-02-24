1570442a0a89383549c0720f4a4eb422d081143e
Mario Juarez <mario@mjp.one>
2018-02-24 11:29:18 +0100

    Add map screen

--- App/Containers/MapScreen.js
+++ App/Containers/MapScreen.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import { ScrollView, Text, Image, View, Button } from 'react-native'
+import { ScrollView, Text, Image, View } from 'react-native'
 import { Images } from '../Themes'
 
 // Styles
 import styles from './Styles/LaunchScreenStyles'
 
-export default class LaunchScreen extends Component {
+export default class MapScreen extends Component {
   render () {
     return (
       <View style={styles.mainContainer}>
@@ -21,8 +21,6 @@ export default class LaunchScreen extends Component {
               This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
             </Text>
           </View>
-
-          <Button block style={styles.button} onPress={() => this.props.navigation.navigate('MapScreen')} title={'Maps'} />
 
         </ScrollView>
       </View>
--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,11 +1,13 @@
 import { StackNavigator } from 'react-navigation'
 import LaunchScreen from '../Containers/LaunchScreen'
+import MapScreen from '../Containers/MapScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
-  LaunchScreen: { screen: LaunchScreen }
+  LaunchScreen: { screen: LaunchScreen },
+  MapScreen: { screen: MapScreen }
 }, {
   // Default config for all screens
   headerMode: 'none',
