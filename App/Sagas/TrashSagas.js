e00190b9d0af1b4dadcd33f0a3e4913da092740d
Mario Juarez <mario@mjp.one>
2018-02-24 16:56:37 +0100

    Update API call

--- App/Sagas/TrashSagas.js
+++ App/Sagas/TrashSagas.js
@@ -14,7 +14,7 @@ export function* requestLocations(action) {
   bodyFormData.append('lon', geoPosition.longitude);
   bodyFormData.append('trash_types', trashTypes.join(','));
 
-  const response = yield axios.post(
+  const { response, error } = yield axios.post(
     Secrets.API_URL,
     bodyFormData,
     {
@@ -22,18 +22,22 @@ export function* requestLocations(action) {
         'Content-Type': 'multipart/form-data'
       }
     })
-    .then(response => response)
-    .catch(error => error)
+    .then(response => ({ response }))
+    .catch(error => ({ error }))
 
-  if (response.data) {
-    const results = response.data.dist.filter((el, index) => index < LIMIT);
+  if (response) {
+    let locations = [
+      ...response.data.battery_recycling_points.slice(0, 4),
+      ...response.data.clean_points.slice(0, 4),
+      ...response.data.dog_shits.slice(0, 4),
+    ]
 
-    yield put(TrashActions.locationsSuccess(results))
+    yield put(TrashActions.locationsSuccess(locations))
+
+    yield put(NavigationActions.navigate({
+      routeName: 'MapScreen'
+    }))
   } else {
     yield put(TrashActions.locationsFailure(error))
   }
-
-  yield put(NavigationActions.navigate({
-    routeName: 'MapScreen'
-  }))
 }
