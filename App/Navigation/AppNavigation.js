05bd3b1335fc572d3aeacfdecc42b027fcc2fea8
Guillermo de la Puente <g.puente.allott@gmail.com>
2018-02-24 12:39:02 +0100

    Go back to launchscreen initially

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -11,7 +11,7 @@ const PrimaryNav = StackNavigator({
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'MapScreen',
+  initialRouteName: 'LaunchScreen',
   navigationOptions: {
     headerStyle: styles.header
   }
